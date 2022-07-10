b4d4ce6a900a772101774bf8a7509fdca11f15b4
nawm77 <alpha050603@gmail.com>
2022-07-10 22:22:26 +0300

    Commit 1

--- src/main/java/com/ilya/springBootCar/model/Moto.java
+++ src/main/java/com/ilya/springBootCar/model/Moto.java
@@ -3,6 +3,7 @@ package com.ilya.springBootCar.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -11,6 +12,7 @@ public class Moto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min = 1, max = 30)
     @Column(name="make")
     private String make;
     @Column(name = "model")
